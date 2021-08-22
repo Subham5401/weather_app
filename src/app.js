59fb04175352316ce7d85445cbc5e6326517db88
Subham Mishra <mishrasubham21081998@gmail.com>
2021-08-22 17:46:25 +0530

    adding views

--- src/app.js
+++ src/app.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000;
 const static_path = path.join(__dirname, '../public');
 
 app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static(static_path));
 
 // routing
